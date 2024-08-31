3808b590474d6c19a68082a333a1ce1436f7a75e
Oleh Halchenko <oleh.halchenko@appsdelivered.com>
2024-08-31 23:08:46 +0200

    TESTS

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { configDefaults } from "vitest/config";
 
 export default defineConfig({
   plugins: [react()],
+
   server: {
     proxy: {
       "/upload": {
@@ -14,6 +16,13 @@ export default defineConfig({
         },
       },
     },
+  },
+
+  test: {
+    environment: "jsdom",
+    globals: true,
+    setupFiles: "./vitest.setup.ts",
+    exclude: [...configDefaults.exclude, "e2e/*"],
   },
 });
 
